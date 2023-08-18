0341d9333adab2012ba1af95ad5dc9776c2ceabb
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2023-08-18 12:00:54 -0700

    added page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,14 @@
-
-import Link from 'next/link'
-
-import {BiHomeCircle, BiUser} from 'react-icons/bi'
-import {BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots} from 'react-icons/bs'
-import {HiOutlineHashtag} from 'react-icons/hi'
-
-const NAVIGATION_ITEMS = [
-  {
-    title: "Twitter",
-    icon: BsTwitter
-  },
-  {
-    title: "Home",
-    icon: BiHomeCircle
-  },
-  {
-    title: "Explore",
-    icon: HiOutlineHashtag
-  },
-  {
-    title: "Notifications",
-    icon: BsBell
-  },
-  {
-    title: "Messages",
-    icon: BsEnvelope
-  },
-  {
-    title: "Bookmarks",
-    icon: BsBookmark
-  },
-  {
-    title: "Profile",
-    icon: BiUser
-  }
-]
+import LeftSideBar from "@/components/LeftSideBar"
+import MainComponent from "@/components/MainComponent"
+import RightSideBar from "@/components/RightSideBar"
 
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-xl w-full h-full flex relative">
-        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
-          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
-          {
-            NAVIGATION_ITEMS.map((item) => (
-              <Link key={item.title} className="hover:bg-white/10 text-2xl duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6" href={`/${item.title.toLowerCase()}`}>
-                <div>
-                  <item.icon />
-                </div>
-                  {item.title !== "Twitter" && <div>{item.title}</div>}
-              </Link>
-            ))
-          }
-          <button className='rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition:duration-200'>
-            Tweet
-          </button>
-          </div>
-          <button className='rounded-full flex items-center  space-x-2 m-4 bg-transparent p-4 text-2xl text-center hover:bg-white/20 transition:duration-200 w-full justify-between'>
-            <div className='flex items-center space-x-2'>
-            <div className='rounded-full bg-slate-400 w-10 h-10'></div>
-            <div className='text-left text-sm'>
-              <div className='font-semibold'>Eli Derjabin</div>
-              <div className=''>@derjabineli</div>
-            </div>
-            <div></div>
-            </div>
-            <BsThreeDots />
-          </button>
-        </section>
-        <main></main>
-        <section></section>
+      <div className="max-w-[70vw] w-full h-full flex relative">
+        <LeftSideBar />
+        <MainComponent />
+        <RightSideBar />
       </div>
     </div>
   )
--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -38,8 +38,8 @@ const NAVIGATION_ITEMS = [
 
 const LeftSideBar = () => {
   return (
-    <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
-          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
+    <section className="sticky top-0 w-[23%] flex flex-col items-stretch h-screen">
+          <div className='flex flex-col items-stretch h-full space-y-4 mt-4 px-4'>
           {
             NAVIGATION_ITEMS.map((item) => (
               <Link key={item.title} className="hover:bg-white/10 text-2xl duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6" href={`/${item.title.toLowerCase()}`}>
@@ -50,11 +50,11 @@ const LeftSideBar = () => {
               </Link>
             ))
           }
-          <button className='rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition:duration-200'>
+          <button className='rounded-full bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition:duration-200'>
             Tweet
           </button>
           </div>
-          <button className='rounded-full flex items-center  space-x-2 m-4 bg-transparent p-4 text-2xl text-center hover:bg-white/20 transition:duration-200 w-full justify-between'>
+          <button className='rounded-full flex items-center space-x-2 bg-transparent p-4 text-center hover:bg-white/10 transition duration-200 w-full justify-between'>
             <div className='flex items-center space-x-2'>
             <div className='rounded-full bg-slate-400 w-10 h-10'></div>
             <div className='text-left text-sm'>
